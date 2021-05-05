fac96674bfdaaf8f93d4d9373aea83eb61f15134
Moises Nervis <StarCommand@Moisess-iMac.local>
2021-05-05 08:23:48 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,55 @@
+const express = require('express')
+const path = require('path')
+
+const shopRouters = require('./routes/shop')
+
+const app = express()
+
+//middlewares
+// app.use((req, res, next) => {
+//     console.log('In the first middleware')
+//     next() //allows the request to continue to the next middleware
+// })
+
+// app.use('/hello', (req,res,next) => {
+//     console.log('In the second middleware')
+//     res.send('<h1>Hello from Express.js</h1>')
+// })
+
+// app.get('/', (req,res) => {
+//     res.sendFile(path.join(__dirname, 'public', 'index.html'))
+// })
+
+//to parse incoming form data --body parser
+app.use(express.urlencoded({ extended: false }))
+
+app.use('/add-products', (req, res) => {
+    res.send(`
+        <form action="/product" method="POST">
+            <input type="text" name="title" />
+            <button type="submit">SEND</button>
+        </form>
+    `)
+})
+
+app.use('/product', (req, res) => {
+    console.log(req.body)
+    res.redirect('/')
+})
+
+// route
+app.use(shopRouters)
+
+// app.use((req, res,next)=>{
+//     res.status(404).send('<h1> Page not found </h1>');
+//  });
+
+//catch-all-middleware ------ 404
+
+app.use('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', '404.html'))
+})
+
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => console.log(`Server started at port ${PORT}`))
